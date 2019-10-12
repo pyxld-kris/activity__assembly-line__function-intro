43aca3411675bc860aaaa09d880254b45ead01d8
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-10-12 10:32:42 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# activity__assembly-line__function-intro
-Created with CodeSandbox
+# DEV LAUNCHERS ACTIVITY
+
+### Stage Maker - Array Intro
+
+We're going to be modifying the "modify.mjs" file for this activity!
+
+Key Concepts: arrays, loops
+
+---
+
+#### GOAL:
+
+Sort the steps to allow your character to reach the goal!
+
+Open the "modify.mjs" file to begin
+
+---
---
+++ modify.mjs
@@ -0,0 +1,52 @@
+/**** WELCOME! *********************************************/
+/**
+ * Sort the steps to allow your character to reach the goal!
+ * 
+ * 
+ * ----------------------------------------------------------
+ * After making a change: save this file, then press the refresh
+ * button above the game window!
+ * ----------------------------------------------------------
+ */
+
+/**************** Start Modifying Here! *********************/
+
+
+this.sortStairs = function(array) {
+
+  // Finish the incomplete sorting algorithm!
+  for (let i=0; i<array.length; i++) {
+    swapElements(i, i-1);
+  }
+
+}
+
+/**** GOODBYE! ************************************************/
+/**** Stop Modifying Here! (Unless you want to experiment!) ***/
+
+/* */
+/*/
+/*/
+/*/
+/*/
+/*/
+/*/
+/*/
+/*/
+/*/
+/* */
+
+var scene = this;
+function swapElements(indexA, indexB) {
+  if (indexA<0 || indexB<0 || indexA >=scene.stageArray.length || indexB >=scene.stageArray.length)
+    return;
+  // Swap the integers in stageArray
+  let temp = scene.stageArray[indexA];
+  scene.stageArray[indexA] = scene.stageArray[indexB];
+  scene.stageArray[indexB] = temp;
+
+  return 1;
+}
+
+/* eslint-disable */ // Stops codesandbox from giving us annoying errors
+
